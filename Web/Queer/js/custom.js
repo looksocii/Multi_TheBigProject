a7d010972e2dcd5d71d24f771e33f1ac2d2a5e71
61070139 <61070139@kmitl.ac.th>
2019-05-25 03:59:07 +0700

    queer

--- Web/Queer/js/custom.js
+++ Web/Queer/js/custom.js
@@ -61,3 +61,14 @@ $(window).load(function () {
         }
     });
 });
+
+$(window).on('scroll', function(){
+    if($(window).scrollTop()) {
+        $('nav').addClass('black');
+        //$('nav').removeClass('white');
+    }
+    else {
+        $('nav').removeClass('black');
+        //$('nav').addClass('white');
+    }
+});
